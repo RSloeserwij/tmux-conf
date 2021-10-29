cf6f0766f562274c0f30e37a15b09337c55e78c3
roys <roys@quanza.net>
2021-10-29 12:55:15 +0200

    init

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# $1    -> desired file location
+main() {
+	FILE="$HOME/.tmux.conf"
+	if [[ $# -gt 0 ]]; then
+		FILE="$1"
+	fi
+	
+	if file_exists $FILE; then
+		backup_file $FILE
+	fi
+	place_file $FILE
+}
+
+# $1    -> desired file location
+file_exists() {
+	return [ -f "$1" ]
+}
+
+
+# $1    -> desired file location
+place_file() {
+	mv "$SCRIPT_DIR/tmux.conf" "$1"
+}
+
+# $1    -> file to back up
+backup_file() {
+	mv $1 "$1.old"
+}
+
